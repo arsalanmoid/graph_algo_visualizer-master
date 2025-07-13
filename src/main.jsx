17f677d499edc615d8e0e8b2ca53e7a83af45f69
arsalan <arsalanmoid1@gmail.com>
2025-07-13 13:33:46 +0530

    initial

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+// import './Handlers/patchPassiveEvents.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
